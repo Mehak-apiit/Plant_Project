9ddd6acdde99dc8944e1156bc5dcadbcd6649919
Mehak <mehak.apiit@gmail.com>
2026-05-27 11:41:12 +0530

    Login API with JWT working

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -21,6 +22,41 @@ export const registerUser = async (req, res) => {
       message: "User registered successfully",
       user,
     });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // CHECK USER EXISTS
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // CHECK PASSWORD
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // CREATE JWT TOKEN
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user,
+    });
+
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { registerUser } from "../controllers/authController.js";
+import { registerUser, loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
 // REGISTER ROUTE
 router.post("/register", registerUser);
+// LOGIN ROUTE
+router.post("/login", loginUser);
 
 export default router;
