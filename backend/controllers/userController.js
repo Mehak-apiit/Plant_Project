5693dde16b005cab27ae9abe9c3fcb09b69d4c5c
Mehak <mehak.apiit@gmail.com>
2026-05-30 22:22:59 +0530

    Implemented admin user management (get users, delete user, update role) with role-based access control

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,3 +14,64 @@ export const getAllUsers = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+
+export const deleteUser = async (req, res) => {
+  try {
+    // 1. USER FIND KARO ID SE
+    const user = await User.findById(req.params.id);
+
+    // 2. AGAR USER NAHI MILA
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // 3. USER DELETE KARO
+    await user.deleteOne();
+
+    // 4. RESPONSE
+    res.json({ message: "User deleted successfully" });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+// @desc   Update user role (Admin)
+// @route  PUT /api/users/:id/role
+// @access Private/Admin
+
+export const updateUserRole = async (req, res) => {
+  try {
+    const { role } = req.body;
+
+    // 1. VALIDATION
+    if (!role) {
+      return res.status(400).json({ message: "Role is required" });
+    }
+
+    if (!["user", "admin", "staff"].includes(role)) {
+      return res.status(400).json({ message: "Invalid role" });
+    }
+
+    // 2. USER FIND
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // 3. UPDATE ROLE
+    user.role = role;
+
+    await user.save();
+
+    // 4. RESPONSE
+    res.json({
+      message: "User role updated successfully",
+      user,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,17 @@ import express from "express";
 import { getAllUsers } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { admin } from "../middleware/roleMiddleware.js";
+import { deleteUser,updateUserRole } from "../controllers/userController.js";
 
 const router = express.Router();
 
 // ADMIN: GET ALL USERS
 router.get("/", protect, admin, getAllUsers);
 
+// DELETE USER (ADMIN)
+router.delete("/:id", protect, admin, deleteUser);
+
 export default router;
+
+// UPDATE ROLE (ADMIN)
+router.put("/:id/role", protect, admin, updateUserRole);
